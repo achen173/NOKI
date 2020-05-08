93903e910b8edccbfa516d951e30f14559cedec3
achen173 <54816128+achen173@users.noreply.github.com>
2020-05-08 14:27:38 -0400

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from PyExifTool import exiftool
-files = "hello.docx"
-with exiftool.ExifTool() as et:
-    metadata = et.get_metadata(files)
-for d in metadata:
-    print("{:20.20} {:20.20}".format(d["SourceFile"],
-                                         d["EXIF:DateTimeOriginal"]))
